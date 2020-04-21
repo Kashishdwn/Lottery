e3e9920797765c9070a103759a4d17a0016691d2
Kashishdwn <kashishdwn@gmail.com>
2020-04-22 01:44:41 +0530

    Updates

---
+++ precache-manifest.251f3b51ae545935d9ad15007effc833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a876cd6c1879bbf9056792e57b43f52",
+    "url": "/Lottery/index.html"
+  },
+  {
+    "revision": "f09304c1931a53370326",
+    "url": "/Lottery/static/css/main.0f9ca952.chunk.css"
+  },
+  {
+    "revision": "74b60e5854ef64d67125",
+    "url": "/Lottery/static/js/2.625b6834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lottery/static/js/2.625b6834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09304c1931a53370326",
+    "url": "/Lottery/static/js/main.cef48a19.chunk.js"
+  },
+  {
+    "revision": "d82182c96a511fbd3606",
+    "url": "/Lottery/static/js/runtime-main.3555bdf3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Lottery/static/media/logo.5d5d9eef.svg"
+  }
+]);
